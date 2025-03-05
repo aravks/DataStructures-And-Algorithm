a03ed9a6d82fde22321ddd9cc56192c033990014
Aravind K S <aravindvasan1@gmail.com>
2025-03-05 11:09:33 +0530

    Create bubbleSort.js

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+let arr = [6,5,4,3,21]
+
+
+
+function bubbleSort(arr) {
+    for(let i = arr.length - 1; i > 0 ; i--) {
+        for(let j = 0; j < i; j++) {
+            if(arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort(arr))
