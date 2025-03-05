6362a62576f40e407395f8c097ee8af53a3307b3
Aravind K S <aravindvasan1@gmail.com>
2025-03-05 11:19:09 +0530

    Update bubbleSort.js

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -3,8 +3,8 @@ let arr = [6,5,4,3,21]
 
 
 function bubbleSort(arr) {
-    for(let i = arr.length - 1; i > 0 ; i--) {
-        for(let j = 0; j < i; j++) {
+    for(let pass = arr.length - 1; pass > 0 ; pass--) {
+        for(let j = 0; j < pass; j++) {
             if(arr[j] > arr[j+1]) {
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
             }
