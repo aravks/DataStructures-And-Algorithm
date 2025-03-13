727b03c76b96a8d97679de062cf651e5e2f4b0c6
Aravind K S <aravindvasan1@gmail.com>
2025-03-13 21:39:10 +0530

    Create ratMazePrintPath.js

---
+++ Recursion/ratMazePrintPath.js
@@ -0,0 +1,16 @@
+function ratMazePrintPath(row, col, path = '') {
+    if(row === 1 && col === 1) {
+        console.log(path)
+        return
+    }
+    if(row > 1) {
+        let down = ratMazePrintPath(row - 1, col, path + 'D')
+    }
+    if(col > 1) {
+        let right = ratMazePrintPath(row, col - 1, path + 'R')
+    }
+    
+    // return down + right
+}
+
+ratMazePrintPath(3, 3)
