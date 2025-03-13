d52f26009767221f8b16a9d44b226f28e63c6eec
Aravind K S <aravindvasan1@gmail.com>
2025-03-13 21:32:31 +0530

    Create ratMazeCountPath.js

---
+++ Recursion/ratMazeCountPath.js
@@ -0,0 +1,10 @@
+function ratMazeCountPath(row, col) {
+    if(row === 1 || col === 1) return 1
+    
+    let down = ratMazeCountPath(row - 1, col)
+    let right = ratMazeCountPath(row, col - 1)
+    
+    return down + right
+}
+
+console.log(ratMazeCountPath(3, 3))
